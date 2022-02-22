fd167bd17977704da8cd8f12512f6af06662cbcd
ashish <aa54035@yahoo.com>
2022-02-22 13:20:57 +0530

    Owner message comp completed

--- src/Components/MyMessage.js
+++ src/Components/MyMessage.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const MyMessage = () => {
-  return (
-    <div>MyMessage</div>
-  )
+const MyMessage = ({ message }) => {
+
+    if (message.attachments && message.attachments.length > 0) {
+        return (<img src={message.attachment[0].file}
+            alt="message-attachment"
+            className='message-image'
+            style={{ float: 'right' }}
+        />);
+    }
+
+    return (
+        <div>
+            <div className="messages" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3b5A20' }}>
+                {message.text}
+            </div>
+
+        </div>
+    )
 }
 
 export default MyMessage
